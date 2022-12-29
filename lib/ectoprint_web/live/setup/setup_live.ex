39a53cae9eae32bc302f39d9d0fe73f258eb8ad1
katibest <katibest@launchscout.com>
2022-12-29 11:44:09 -0500

    styled setup page

--- lib/ectoprint_web/live/setup/setup_live.ex
+++ lib/ectoprint_web/live/setup/setup_live.ex
@@ -7,10 +7,86 @@ defmodule EctoprintWeb.SetupLive do
   end
 
   @impl true
+  def handle_params(params, _uri, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("close_modal", _, socket) do
+
+    # Go back to the :index live action
+    {:noreply, push_patch(socket, to: "/")}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex">
-      Setup
+    <div class="mt-9 w-full p-14">
+      <.h1>
+        <span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Fancy Setup
+        </span>
+      </.h1>
+      <.button type="live_patch" to="/setup/modal" label="Upload Files"/>
+      <%= if @live_action == :modal do %>
+      <.modal title="Modal" close_modal_target={@myself}>
+        <.p>Content</.p>
+      </.modal>
+      <% end %>
+      <.table class="mt-5">
+        <.tr>
+          <.th>File Name</.th>
+          <.th>Size</.th>
+          <.th>Date Uploaded</.th>
+          <.th></.th>
+        </.tr>
+
+        <.tr>
+          <.td>
+            Cat_file_103.cd
+          </.td>
+          <.td>20gb</.td>
+          <.td>02/03/04</.td>
+          <.td class="float-right flex">
+            <HeroiconsV1.Solid.folder class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.bookmark class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.trash class="w-6 h-6 text-secondary-500" />
+          </.td>
+        </.tr>
+
+        <.tr>
+          <.td>Cat_file_109.cd</.td>
+          <.td>20gb</.td>
+          <.td>02/03/04</.td>
+          <.td class="float-right flex">
+            <HeroiconsV1.Solid.folder class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.bookmark class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.trash class="w-6 h-6 text-secondary-500" />
+          </.td>
+        </.tr>
+
+        <.tr>
+          <.td>Cat_file_223.cd</.td>
+          <.td>20gb</.td>
+          <.td>02/03/04</.td>
+          <.td class="float-right flex">
+            <HeroiconsV1.Solid.folder class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.bookmark class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.trash class="w-6 h-6 text-secondary-500" />
+          </.td>
+        </.tr>
+
+        <.tr>
+          <.td>Cat_file_143.cd</.td>
+          <.td>20gb</.td>
+          <.td>02/03/04</.td>
+          <.td class="float-right flex">
+            <HeroiconsV1.Solid.folder class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.bookmark class="w-6 h-6 text-secondary-500" />
+            <HeroiconsV1.Solid.trash class="w-6 h-6 text-secondary-500" />
+          </.td>
+        </.tr>
+      </.table>
     </div>
     """
   end
