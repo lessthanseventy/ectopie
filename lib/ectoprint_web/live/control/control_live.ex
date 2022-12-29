8d20cb5545ad7104e63bc7ebab5ea6871465ac31
katibest <katibest@launchscout.com>
2022-12-29 11:44:00 -0500

    styled control page

--- lib/ectoprint_web/live/control/control_live.ex
+++ lib/ectoprint_web/live/control/control_live.ex
@@ -9,8 +9,40 @@ defmodule EctoprintWeb.ControlLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex">
-      Control
+    <div class="mt-9 w-full p-14">
+      <.h1>
+        <span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Fancy Control
+        </span>
+      </.h1>
+      <.card class="max-w-lg" variant="outline">
+        <.card_content category="Article" heading="Enhance your Phoenix development">
+          <.dropdown label="Dropdown">
+              <.dropdown_menu_item>
+                <HeroiconsV1.Outline.home class="w-5 h-5 text-gray-500" />
+                Button item with icon
+                </.dropdown_menu_item>
+                <.dropdown_menu_item link_type="button" label="button item" class="block" />
+                <.dropdown_menu_item link_type="a" to="/" label="Link" />
+                <.dropdown_menu_item link_type="a" to="/" label="Link with method" method={:put} />
+                <.dropdown_menu_item link_type="live_patch" to="/" label="Live Patch" />
+                <.dropdown_menu_item link_type="live_redirect" to="/" label="Live Redirect" />
+                <.dropdown_menu_item phx-click="some_phoenix_click_event" label="Phoenix click event" />
+               </.dropdown>
+              <.dropdown label="Dropdown">
+            <.dropdown_menu_item>
+              <HeroiconsV1.Outline.home class="w-5 h-5 text-gray-500" />
+              Button item with icon
+            </.dropdown_menu_item>
+            <.dropdown_menu_item link_type="button" label="button item" />
+            <.dropdown_menu_item link_type="a" to="/" label="Link" />
+            <.dropdown_menu_item link_type="a" to="/" label="Link with method" method={:put} />
+            <.dropdown_menu_item link_type="live_patch" to="/" label="Live Patch" />
+            <.dropdown_menu_item link_type="live_redirect" to="/" label="Live Redirect" />
+            <.dropdown_menu_item phx-click="some_phoenix_click_event" label="Phoenix click event" />
+          </.dropdown>
+        </.card_content>
+      </.card>
     </div>
     """
   end
